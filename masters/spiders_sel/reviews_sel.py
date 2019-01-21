ea0e0aaf7416fe8a79da3ec36ef911ae5462402a
Nejc Ribic <nejc.ribic95@gmail.com>
2019-01-21 18:49:10 +0100

    selenium fix

--- masters/spiders_sel/reviews_sel.py
+++ masters/spiders_sel/reviews_sel.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
 def get_coordinates(url):
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    # chrome_options.binary_location = '/opt/google/chrome/google-chrome'
+    # service_log_path = "{}/chromedriver.log".format("/home/nejc/Desktop/scrapers/trip/")
+    service_args = ['--verbose']
+
+    driver = webdriver.Chrome(
+        chrome_options=chrome_options,
+        service_args=service_args,
+        # service_log_path=service_log_path
+    )
     driver.get(url)
     driver.implicitly_wait(5)
 
     coord_url = driver.find_element_by_css_selector("div.staticMap img").get_attribute("src")
     print(coord_url)
-    # elem = driver.find_element_by_name("q")
-    # elem.clear()
     driver.close()
     return coord_url
