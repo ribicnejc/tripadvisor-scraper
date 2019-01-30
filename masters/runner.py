acc146640baa8c8a33a796fa5984aa86946f8e48
Nejc Ribic <nejc.ribic95@gmail.com>
2019-01-30 21:43:26 +0100

    remove useless libs

--- masters/runner.py
+++ masters/runner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from scrapy import cmdline
-from masters import settings
+#from masters import settings
 
 # cmdline.execute("scrapy crawl locations".split())
 cmdline.execute("scrapy crawl reviews".split())
