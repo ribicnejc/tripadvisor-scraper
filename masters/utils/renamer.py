780c62d4d0b089254c950369ed2910533fd0a5e3
Nejc Ribic <nejc@tiddlylabs.com>
2019-09-21 14:43:56 +0200

    renamed for ntsf

---
+++ masters/utils/renamer.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+# <>:"/\|?*
+folder = "../scraped_data/data_reviews_italy"
+for file in os.listdir(folder):
+    file_old = file
+    if file.__contains__("<"):
+        file = file.replace("<", "_")
+    if file.__contains__(">"):
+        file = file.replace(">", "_")
+    if file.__contains__(":"):
+        file = file.replace(":", "_")
+    if file.__contains__("\""):
+        file = file.replace("\"", "_")
+    if file.__contains__("/"):
+        file = file.replace("/", "_")
+    if file.__contains__("\\"):
+        file = file.replace("\\", "_")
+    if file.__contains__("|"):
+        file = file.replace("|", "_")
+    if file.__contains__("?"):
+        file = file.replace("?", "_")
+    if file.__contains__("*"):
+        file = file.replace("*", "_")
+
+    if file_old != file:
+        os.rename(folder + "/" + file_old, folder + "/" + file)
+        print(file_old)
