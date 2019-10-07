96fead54e73b0f6fe4fcb2f5bc1ca98c49e50250
Nejc Ribic <nejc.ribic95@gmail.com>
2019-10-07 19:53:36 +0200

    finished infomap parser

--- masters/infomap/infomap_data.py
+++ masters/infomap/infomap_data.py
@@ -31,19 +31,47 @@ def get_attractions_cursor():
     return cur.fetchall()
 
 
-def get_pajek_format():
-    attractions = []
-    for attraction in get_attractions_cursor():
-        attractions.append(attraction[0])
-
+def save_pajek_format(vertices, edges):
     filename = 'infomap_data/slovenia.net'
-    with open(filename, 'a+') as f:
-        f.write("*Vertices " + str(attractions.__len__()) + "\n")
-        i = 1
-        for attraction in attractions:
-            f.write(str(i) + " " + attraction.split("-Reviews-")[1] + " 1.0\n")
-            i += 1
-    # for review in get_reviews_cursor():
+    with open(filename, 'w+') as f:
+        f.write("*Vertices " + str(vertices.__len__()) + "\n")
+        for vertice in vertices.items():
+            f.write(str(vertice[1]) + " \"" + vertice[0].split("-Reviews-")[1] + "\" 1.0\n")
+        f.write("*Edges " + str(edges.__len__()) + "\n")
+        for edge in edges.items():
+            f.write(edge[0] + " " + str(edge[1]) + "\n")
+
+
+def get_key_from_locations(l1, l2, locations):
+    k1 = locations[l1]
+    k2 = locations[l2]
+    if k1 < k2:
+        return str(k1) + " " + str(k2)
+    return str(k2) + " " + str(k1)
+
+
+def get_pajek_format():
+    attractions = {}
+    i = 1
+    for attraction in get_attractions_cursor():
+        attractions.__setitem__(attraction[0], i)
+        i += 1
+
+    edges = {}
+    prev = None
+    for review in get_reviews_cursor():
+        if prev is None:
+            prev = review
+            continue
+        if review[0] == prev[0]:  # if is the same user
+            if int(review[2]) - int(prev[2]) < 30:  # if time between visit is less then 30
+                key = get_key_from_locations(review[5], prev[5], attractions)  # get key for edges
+                if edges.get(key):
+                    edges.__setitem__(key, edges.get(key) + 1)
+                else:
+                    edges.__setitem__(key, 1)
+        prev = review
+    save_pajek_format(attractions, edges)
 
 
 get_pajek_format()
