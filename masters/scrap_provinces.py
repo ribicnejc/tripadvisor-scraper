f1b844190aa264b2e94c1beaf0db850eb2047dc5
Nejc Ribic <nejc.ribic95@gmail.com>
2021-01-03 21:30:36 +0100

    All provinces

--- masters/scrap_provinces.py
+++ masters/scrap_provinces.py
@@ -6,4 +6,9 @@ sys.path.append("..")
 
 print("Scraper started...")
 
-cmdline.execute("scrapy crawl provinces -a country=/Attractions-g294473-Activities-oa20-Ukraine.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g294473-Activities-oa20-Ukraine.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g274862-Activities-oa20-Slovenia.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g294453-Activities-oa20-Croatia.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g274881-Activities-oa20-Hungary.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g190410-Activities-oa20-Austria.html".split())
+# cmdline.execute("scrapy crawl provinces -a country=/Attractions-g187768-Activities-oa20-Italy.html".split())
