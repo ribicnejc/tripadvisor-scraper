290c2bb75d1504a47a8cda8873fd91cf0ac4599d
Nejc Ribic <nejc.ribic95@gmail.com>
2021-01-06 09:29:36 +0100

    Scraped locations

--- masters/data_managers/squash_data.py
+++ masters/data_managers/squash_data.py
@@ -6,13 +6,13 @@ locations = "locations_" + settings.COUNTRY + ".zip"
 provinces = "provinces_" + settings.COUNTRY + ".zip"
 reviews = "reviews_" + settings.COUNTRY + ".zip"
 
-with ZipFile(provinces, 'w') as zipProvinces:
-    root = "../scraped_data/data_provinces/" + settings.COUNTRY
-    files = listdir(root)
-    print("Zipping provinces...")
-    for file in files:
-        zipProvinces.write(root + "/" + file)
-    print("Provinces zipped.")
+# with ZipFile(provinces, 'w') as zipProvinces:
+#     root = "../scraped_data/data_provinces/" + settings.COUNTRY
+#     files = listdir(root)
+#     print("Zipping provinces...")
+#     for file in files:
+#         zipProvinces.write(root + "/" + file)
+#     print("Provinces zipped.")
 
 with ZipFile(locations, 'w') as zipLocations:
     root = "../scraped_data/data_locations/" + settings.COUNTRY
@@ -22,10 +22,10 @@ with ZipFile(locations, 'w') as zipLocations:
         zipLocations.write(root + "/" + file)
     print("Locations zipped.")
 
-with ZipFile(reviews, 'w') as zipReviews:
-    root = "../scraped_data/data_reviews/" + settings.COUNTRY
-    files = listdir(root)
-    print("Zipping reviews...")
-    for file in files:
-        zipReviews.write(root + "/" + file)
-    print("Reviews zipped.")
+# with ZipFile(reviews, 'w') as zipReviews:
+#     root = "../scraped_data/data_reviews/" + settings.COUNTRY
+#     files = listdir(root)
+#     print("Zipping reviews...")
+#     for file in files:
+#         zipReviews.write(root + "/" + file)
+#     print("Reviews zipped.")
