ace212c98bea8bac50e5c9cb8c03ad9ec499fba9
Nejc Ribic <nejc.ribic95@gmail.com>
2021-01-25 13:42:15 +0100

    Performance scraping RAM limitations

--- masters/data_managers/squash_data.py
+++ masters/data_managers/squash_data.py
@@ -2,25 +2,25 @@ from zipfile import ZipFile
 from os import listdir
 from masters import settings
 
-locations = "../data/locations/locations_" + settings.COUNTRY + ".zip"
-provinces = "../data/provinces/provinces_" + settings.COUNTRY + ".zip"
-reviews = "../data/reviews/reviews_" + settings.COUNTRY + ".zip"
+# locations = "../data/locations/locations_" + settings.COUNTRY + ".zip"
+# provinces = "../data/provinces/provinces_" + settings.COUNTRY + ".zip"
+reviews = "../data/reviews/reviews_" + settings.COUNTRY + "_1.zip"
 
-with ZipFile(provinces, 'w') as zipProvinces:
-    root = "../scraped_data/data_provinces/" + settings.COUNTRY
-    files = listdir(root)
-    print("Zipping provinces...")
-    for file in files:
-        zipProvinces.write(root + "/" + file)
-    print("Provinces zipped.")
-
-with ZipFile(locations, 'w') as zipLocations:
-    root = "../scraped_data/data_locations/" + settings.COUNTRY
-    files = listdir(root)
-    print("Zipping locations...")
-    for file in files:
-        zipLocations.write(root + "/" + file)
-    print("Locations zipped.")
+# with ZipFile(provinces, 'w') as zipProvinces:
+#     root = "../scraped_data/data_provinces/" + settings.COUNTRY
+#     files = listdir(root)
+#     print("Zipping provinces...")
+#     for file in files:
+#         zipProvinces.write(root + "/" + file)
+#     print("Provinces zipped.")
+#
+# with ZipFile(locations, 'w') as zipLocations:
+#     root = "../scraped_data/data_locations/" + settings.COUNTRY
+#     files = listdir(root)
+#     print("Zipping locations...")
+#     for file in files:
+#         zipLocations.write(root + "/" + file)
+#     print("Locations zipped.")
 
 with ZipFile(reviews, 'w') as zipReviews:
     root = "../scraped_data/data_reviews/" + settings.COUNTRY
