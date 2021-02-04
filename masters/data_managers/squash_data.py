559884e0f42369bb45b53b878ec292c4514f15e8
Nejc Ribic <nejc.ribic95@gmail.com>
2021-02-04 21:58:48 +0100

    Italy part 4

--- masters/data_managers/squash_data.py
+++ masters/data_managers/squash_data.py
@@ -4,7 +4,7 @@ from masters import settings
 
 # locations = "../data/locations/locations_" + settings.COUNTRY + ".zip"
 # provinces = "../data/provinces/provinces_" + settings.COUNTRY + ".zip"
-reviews = "../data/reviews/reviews_" + settings.COUNTRY + "_4.zip"
+reviews = "../data/reviews/reviews_" + settings.COUNTRY + "_5.zip"
 
 # with ZipFile(provinces, 'w') as zipProvinces:
 #     root = "../scraped_data/data_provinces/" + settings.COUNTRY
