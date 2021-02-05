52aace82a280f5c3ddd4952b799c3d62036183b8
Nejc Ribic <nejc.ribic95@gmail.com>
2021-02-05 23:03:31 +0100

    Prepare for overkill finish

--- masters/settings.py
+++ masters/settings.py
@@ -100,7 +100,7 @@ AUTOTHROTTLE_MAX_DELAY = 10
 
 #############################################################
 # Custom settings
-HEADLESS_MODE = False
+HEADLESS_MODE = True
 COUNTRY = "overkill"  # aus|cro|hun|ita|slo|ukr|overkill
 
 SPLASH_URL = 'http://192.168.99.100:8050'
