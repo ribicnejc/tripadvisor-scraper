8d36f4b44a59de0f47f3ab8377cae9fffdc676a6
Nejc Mac <nejc.ribic95@gmail.com>
2021-09-22 15:47:23 +0200

    Spider net

--- masters/analyser/kilometers/trip_length.py
+++ masters/analyser/kilometers/trip_length.py
@@ -93,39 +93,43 @@ order by user_id, cast(review_id as INTEGER) asc
     # 1 Day, 2 Days, 3 Days, 4-5 Days, 6-10 Days, 11-15 Days, 16+ Days
     title_arr = []
     days_arr = []
-    a, b, c, d, e, f, g = 2, 2, 2, 2, 2, 2, 2
+    k, a, b, c, d, e, f, g = 2, 2, 2, 2, 2, 2, 2, 2
     trips = sorted(trips)
     for lenght in trips:
-        if lenght < 2:
+        if lenght < 1:
             title_arr.append("1")
+            days_arr.append("Trip time")
+            k += 1
+        elif 1 <= lenght < 2:
+            title_arr.append("2")
             days_arr.append("Trip time")
             a += 1
         elif 1 < lenght < 3:
-            title_arr.append("2")
+            title_arr.append("3")
             days_arr.append("Trip time")
             b += 1
         elif 2 < lenght < 4:
-            title_arr.append("3")
+            title_arr.append("4")
             days_arr.append("Trip time")
             c += 1
         elif 3 < lenght < 6:
-            title_arr.append("4-5")
+            title_arr.append("5-6")
             days_arr.append("Trip time")
             d += 1
         elif 5 < lenght < 11:
-            title_arr.append("6-10")
+            title_arr.append("7-11")
             days_arr.append("Trip time")
             e += 1
         elif 10 < lenght < 16:
-            title_arr.append("11-15")
+            title_arr.append("12-16")
             days_arr.append("Trip time")
             f += 1
         elif lenght > 15:
-            title_arr.append("16+")
+            title_arr.append("17+")
             days_arr.append("Trip time")
             g += 1
 
-    all = a + b + c + d + e + f + g
+    all = a + b + c + d + e + f + g + k
 
     a = math.ceil(a / math.log10(a))
     b = math.ceil(b / math.log10(b))
@@ -134,8 +138,9 @@ order by user_id, cast(review_id as INTEGER) asc
     e = math.ceil(e / math.log10(e))
     f = math.ceil(f / math.log10(f))
     g = math.ceil(g / math.log10(g))
+    k = math.ceil(k / math.log10(k))
 
-    all = a + b + c + d + e + f + g
+    all = a + b + c + d + e + f + g + k
 
     a = math.ceil((a * 100) / all)
     b = math.ceil((b * 100) / all)
@@ -144,35 +149,39 @@ order by user_id, cast(review_id as INTEGER) asc
     e = math.ceil((e * 100) / all)
     f = math.ceil((f * 100) / all)
     g = math.ceil((g * 100) / all)
+    k = math.ceil((k * 100) / all)
 
     title_arr = []
     days_arr = []
-    for i in range(a):
+    for i in range(k):
         title_arr.append("1")
         days_arr.append(year)
-
-    for i in range(b):
+    for i in range(a):
         title_arr.append("2")
         days_arr.append(year)
 
-    for i in range(c):
+    for i in range(b):
         title_arr.append("3")
         days_arr.append(year)
 
+    for i in range(c):
+        title_arr.append("4")
+        days_arr.append(year)
+
     for i in range(d):
-        title_arr.append("4-5")
+        title_arr.append("5-6")
         days_arr.append(year)
 
     for i in range(e):
-        title_arr.append("6-10")
+        title_arr.append("7-11")
         days_arr.append(year)
 
     for i in range(f):
-        title_arr.append("11-15")
+        title_arr.append("12-16")
         days_arr.append(year)
 
     for i in range(g):
-        title_arr.append("16+")
+        title_arr.append("17+")
         days_arr.append(year)
 
     f, axes = plt.subplots(1, 1, figsize=(7, 7), sharex=True)
--- masters/analyser/spider_net/spider_net.py
+++ masters/analyser/spider_net/spider_net.py
@@ -1,7 +1,15 @@
 # Libraries
+import math
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from math import pi
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from masters.data_managers.utils import database_utils
+
 
 # TODO relativni graf
 # -- 2020
@@ -25,19 +33,100 @@ from math import pi
 # -- Outdoor Activities,134
 # -- Traveller Resources,116
 # Set data
-sum2019 = 0 + 0 + 498 + 868 + 251 + 99 + 134 + 168
-sum2020 = 0 + 0 + 124 + 115 + 65 + 45 + 35 + 26
-df = pd.DataFrame({
-    'group': ['2019', '2020'],
-    # 'Sights & Landmarks': [6072 / sum2019, 830 / sum2020],
-    # 'Nature & Parks': [4585 / sum2019, 662 / sum2020],
-    'Food & Drink': [498 / sum2019, 124 / sum2020],
-    'Museums': [868 / sum2019, 115 / sum2020],
-    'Spas & Wellness': [251 / sum2019, 65 / sum2020],
-    'Fun & Games': [99 / sum2019, 45 / sum2020],
-    'Outdoor Activities': [134 / sum2019, 35 / sum2020],
-    'Shopping': [168 / sum2019, 26 / sum2020]
-})
+
+
+def get_monthly_visits(country, year_from, year_to, year):
+    sql = """
+        select attraction_name, attraction_url, attraction_type, attraction_rate, review_location_type, count(attraction_url) as visits 
+        from provinces
+            join locations l on provinces.province_url = l.attraction_parent_url
+            join reviews r on l.attraction_url = r.parent_url
+        where review_date > {year_from}
+            and review_date < {year_to}
+        group by attraction_type
+       --order by visits desc
+""".format(country=country, year_from=year_from, year_to=year_to)
+    connection = database_utils.create_connection("../../data/databases/slo_aus_ita_hun_cro_updated.db")
+    data = database_utils.get_data(connection, sql)
+    month_arr = []
+    type_arr = []
+    for e in data:
+        month_arr.append((e[2], e[5]))
+        type_arr.append(year)
+    return month_arr, type_arr
+
+print("1")
+month_arr, type_arr = get_monthly_visits("all", 20190100, 20200100, "2019")
+print("2")
+m, t = get_monthly_visits("all", 20200200, 20210100, "2020")
+print("3")
+data = {}
+data['group'] = ['2019', '2020']
+
+filt = ["None",
+        "Other",
+        "Nightlife",
+        "Sights & Landmarks",
+        "Boat Tours & Water Sports"]
+
+for e in month_arr:
+    if e[0] not in filt:
+        data[e[0]] = [e[1], 0]
+
+for e in m:
+    if e[0] not in filt:
+        if e[0] in data:
+            data[e[0]][1] = e[1]
+        else:
+            data[e[0]] = [0, e[1]]
+
+sum1 = 0
+sum2 = 0
+for k, v in data.items():
+    if "group" in k:
+        continue
+    sum1 += v[0]
+    sum2 += v[1]
+
+for k, v in data.items():
+    if "group" in k:
+        continue
+    s1 = math.log2(sum1)
+    s2 = math.log2(sum2)
+    v1 = math.log2(v[0] + 1)
+    v2 = math.log2(v[1] + 1)
+    r1 = math.ceil((v1 * 100) / s1)
+    r2 = math.ceil((v2 * 100) / s2)
+    relative1 = r1
+    relative2 = r2
+    relative1 = math.log2(math.ceil(v[0] * 100 / sum1) + 1)
+    relative2 = math.log2(math.ceil(v[1] * 100 / sum2) + 1)
+    data[k] = [relative1, relative2]
+
+order = {'group': ['2019', '2020'],
+         "Transportation": [],
+         "Museums": [],
+         "Events": [],
+         "Concerts & Shows": [],
+         "Casinos & Gambling": [],
+         "Shopping": [],
+         "Zoos & Aquariums": [],
+         "Classes & Workshops": [],
+         "Traveller Resources": [],
+         "Fun & Games": [],
+         "Spas & Wellness": [],
+         "Tours": [],
+         "Water & Amusement Parks": [],
+        # "Sights & Landmarks": [],
+         "Outdoor Activities": [],
+         "Nature & Parks": [],
+        # "Boat Tours & Water Sports": [],
+         "Food & Drink": []}
+for k, v in order.items():
+    if k not in data:
+        data[k] = [0, 0]
+    order[k] = data[k]
+df = pd.DataFrame(order)
 
 # ------- PART 1: Create background
 
@@ -61,8 +150,8 @@ plt.xticks(angles[:-1], categories)
 
 # Draw ylabels
 ax.set_rlabel_position(0)
-plt.yticks([0.1, 0.3, 0.5], ["10%", "30%", "50%"], color="grey", size=7)
-plt.ylim(0, 0.5)
+plt.yticks([0.5, 2.5, 6], ["5%", "10%", "30%"], color="grey", size=7)
+plt.ylim(0, 6)
 
 # ------- PART 2: Add plots
 
@@ -82,6 +171,7 @@ ax.plot(angles, values, linewidth=1, linestyle='solid', label="2020")
 ax.fill(angles, values, 'r', alpha=0.1)
 
 # Add legend
-plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-
+plt.xticks(rotation=45)
+plt.legend(loc='upper right', bbox_to_anchor=(0.05, 0.05))
+plt.savefig(f'spider_net.png'.format())
 plt.show()
